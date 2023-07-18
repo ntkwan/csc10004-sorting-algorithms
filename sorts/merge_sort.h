dcc88eae86126469857aa4876fc88b00e92e6a6d
nkwn <npquan2704@gmail.com>
2023-07-18 09:57:34 +0700

    Add namespaces for run-time benchmarking

--- sorts/merge_sort.h
+++ sorts/merge_sort.h
@@ -1,14 +1,19 @@
 #ifndef MERGE_SORT_HEADER
 #define MERGE_SORT_HEADER
 
+namespace runtime {
+    void MergeSort(int a[], int l, int r) {
+        if (l >= r) return;
 
-void MergeSort(int a[], int l, int r) {
-    if (l >= r) return;
+        int mid = (l + r) / 2;
+        MergeSort(a, l, mid);
+        MergeSort(a, mid + 1, r);
+        std::inplace_merge(a + l, a + mid + 1, a + r + 1);
+    }
 
-    int mid = (l + r) / 2;
-    MergeSort(a, l, mid);
-    MergeSort(a, mid + 1, r);
-    std::inplace_merge(a + l, a + mid + 1, a + r + 1);
+    void MergeSort(int a[], int n) {
+        MergeSort(a, 0, n - 1);
+    }
 }
 
 #endif // MERGE_SORT_HEADER
