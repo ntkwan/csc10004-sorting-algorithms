5a2a88e0f6b7c7b16c4a2f331c9caab310569e3b
LilMeen <nhkminh2004@gmail.com>
2023-07-18 14:04:19 +0700

    Add sorts run compare
    Add bubble sort, counting sort, radix sort, shaker sort.

---
+++ src/bubble_sort.h
@@ -0,0 +1,43 @@
+#ifndef BUBBLE_SORT_H_INCLUDED
+#define BUBBLE_SORT_H_INCLUDED
+
+namespace runtime {
+    void BubbleSort(int a[], int n) {
+        int lstExchange = n - 2;
+        for (int i = 0; i < n; i++) {
+            int curExchange = -1;
+            for (int j = 0; j <= lstExchange; j++) {
+                if (a[j] > a[j + 1]) {
+                    std::swap(a[j], a[j + 1]);
+                    curExchange = j;
+                }
+            }
+            if (curExchange == -1) break;
+            else lstExchange = curExchange;
+        }
+    }
+}
+
+namespace runcpr
+{
+    void BubbleSort(int a[], int n, int& comparison)
+    {
+        int lstExchange = n - 2;
+        for (int i = 0; ++comparison && i < n; i++)
+        {
+            int curExchange = -1;
+            for (int j = 0; ++comparison && j <= lstExchange; j++)
+            {
+                if (++comparison && a[j] > a[j + 1])
+                {
+                    std::swap(a[j], a[j + 1]);
+                    curExchange = j;
+                }
+            }
+            if (++comparison && curExchange == -1) break;
+            else lstExchange = curExchange;
+        }
+    }
+}
+
+#endif // BUBBLE_SORT_H_INCLUDED
---
+++ src/shaker_sort.h
@@ -0,0 +1,79 @@
+#ifndef SHAKER_SORT_H_INCLUDED
+#define SHAKER_SORT_H_INCLUDED
+
+namespace runtime {
+	void ShakerSort (int a[], int n) {
+		int left = 0;
+		int right = n - 1;
+		int k = 0;
+
+		while (left < right) {
+			bool isSwapped = false;
+			for (int i = left; i < right; i++) {
+				if (a[i] > a[i + 1]) {
+					std::swap(a[i], a[i + 1]);
+					k = i;
+					isSwapped = true;
+				}
+			}
+
+			if (isSwapped == false) break;
+			right = k;
+
+			isSwapped = false;
+			for (int i = right; i > left; i--) {
+				if (a[i] < a[i - 1]) {
+					std::swap(a[i], a[i - 1]);
+					k = i;
+					isSwapped = true;
+				}
+			}
+
+			if (isSwapped == false) break;
+			left = k;
+		}
+	}
+}
+
+namespace runcpr {
+    void ShakerSort(int a[], int n, int &comparison)
+    {
+        int left = 0;
+        int right = n - 1;
+        int k = 0;
+
+        while (++comparison && left < right)
+        {
+            bool isSwapped = false;
+            for (int i = left; ++comparison && i < right; i++)
+            {
+                if (++comparison && a[i] > a[i + 1])
+                {
+                    std::swap(a[i], a[i + 1]);
+                    k = i;
+                    isSwapped = true;
+                }
+            }
+
+            if (++comparison && isSwapped == false) break;
+            right = k;
+
+            isSwapped = false;
+            for (int i = right; ++comparison && i > left; i--)
+            {
+                if (++comparison && a[i] < a[i - 1])
+                {
+                    std::swap(a[i], a[i - 1]);
+                    k = i;
+                    isSwapped = true;
+                }
+            }
+
+            if (++comparison && isSwapped == false)
+                break;
+
+            left = k;
+        }
+    }
+}
+#endif // SHAKER_SORT_H_INCLUDED
